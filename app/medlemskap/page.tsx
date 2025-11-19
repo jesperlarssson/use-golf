af23c45a54b82caa04d0c4d84a4216c0fe4e12e0
Jesper Larsson <jesper@jepsoncompedit.se>
2025-11-19 12:59:37 +0100

    Gratis custom fitting

--- app/medlemskap/page.tsx
+++ app/medlemskap/page.tsx
@@ -52,7 +52,8 @@ export default function MedlemskapPage() {
                   <ul className="list-disc pl-5 space-y-1 text-sm">
                     <li>10% rabatt på alla bokade tider</li>
                     <li>10% rabatt på USE Golf merch</li>
-                    <li>Förtur till event & ligor</li>
+                    <li>10% rabatt på hela CustomClubs utbud från öppning och fram till 1 maj 2026</li>
+                    <li>Gratis Custom Fitting hos CustomClubs under hela 2026</li>
                     <li>1 timmes speltid ingår vid registrering</li>
                   </ul>
                   <div>
@@ -78,7 +79,8 @@ export default function MedlemskapPage() {
                   <ul className="list-disc pl-5 space-y-1 text-sm">
                     <li>10% rabatt på alla bokade tider</li>
                     <li>10% rabatt på USE Golf merch</li>
-                    <li>Förtur till event & ligor</li>
+                    <li>0% rabatt på hela CustomClubs utbud från öppning och fram till 1 maj 2026</li>
+                    <li>Gratis Custom Fitting hos CustomClubs under hela 2026</li>
                     <li>1 timmes speltid ingår vid registrering</li>
                     <li>Kvalificering till Juniorligan*</li>
 
